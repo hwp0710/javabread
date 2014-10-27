abbc827dfab2cfc520b67309b69f964536923838
weiping-he <weiping-he@PB1ZGB3.na.odcorp.net>
2014-10-27 13:13:10 +0800

    update test

---
+++ Struts2Lesson1/src/test/java/com/shengsiyuan/struts2/LoginActionTest.java
@@ -0,0 +1,14 @@
+package com.shengsiyuan.struts2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginActionTest {
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+}
