48364da2f6c59e543e3ae2502cdcb2c61a18c10c
weiping-he <weiping-he@PB1ZGB3.na.odcorp.net>
2014-10-27 17:59:57 +0800

    add project lesson 6

---
+++ Struts2Lesson6/src/test/java/com/shengsiyuan/action/json/GetJsonActionTest.java
@@ -0,0 +1,14 @@
+package com.shengsiyuan.action.json;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GetJsonActionTest {
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+}
