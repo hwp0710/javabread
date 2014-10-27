4e119a4b09326651d28f8fcb7b614b32e215f06d
weiping-he <weiping-he@PB1ZGB3.na.odcorp.net>
2014-10-27 15:11:03 +0800

    add test

---
+++ Struts2Lesson4/src/test/java/com/shengsiyuan/TestCase.java
@@ -0,0 +1,14 @@
+package com.shengsiyuan;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+}
